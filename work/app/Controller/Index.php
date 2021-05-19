c2ac8baf092742dc9892759f37424ca2da02fb81
kzmeorna <kazuma.engineer@gmail.com>
2021-05-19 09:50:35 +0900

    modified code

--- work/app/Controller/Index.php
+++ work/app/Controller/Index.php
@@ -6,7 +6,7 @@ class Index extends \MyApp\Controller {
 
   public function run(){
     if(isset($_SESSION['me'])){
-      header('Location:'. SITE_URL . '/home.php');
+      header('Location:'. '/home.php');
     }
   }
   
